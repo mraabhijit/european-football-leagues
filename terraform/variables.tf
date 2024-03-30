92dbf50ac424aa67c0c38333af460eac6820b27d
Abhijit Chakraborty <99414748+mraabhijit@users.noreply.github.com>
2024-03-30 12:03:28 +0000

    add terraform code for IAAC

---
+++ terraform/variables.tf
@@ -0,0 +1,41 @@
+variable "credentials" {
+  description = "My Credentials"
+  default     = "../.secret/capstone.json"
+  #ex: if you have a directory where this file is called keys with your service account json file
+  #saved there as my-creds.json you could use default = "./keys/my-creds.json"
+}
+
+
+variable "project" {
+  description = "Project"
+  default     = "dtc-de-course-411615"
+}
+
+variable "region" {
+  description = "Region"
+  #Update the below to your desired region
+  default     = "US"
+}
+
+variable "location" {
+  description = "Project Location"
+  #Update the below to your desired location
+  default     = "US"
+}
+
+variable "bq_dataset_name" {
+  description = "My BigQuery Dataset Name"
+  #Update the below to what you want your dataset to be called
+  default     = "capstone"
+}
+
+variable "gcs_bucket_name" {
+  description = "My Storage Bucket Name"
+  #Update the below to a unique bucket name
+  default     = "capstone-411615"
+}
+
+variable "gcs_storage_class" {
+  description = "Bucket Storage Class"
+  default     = "STANDARD"
+}
